f2a96a36abada485d1db2f0c98fca6d07e3b2f39
pas2rust <pas2000@proton.me>
2025-09-21 22:31:59 -0300

    0.0.8

--- src/components/validator_pattern.rs
+++ src/components/validator_pattern.rs
@@ -1,6 +1,5 @@
 use proc_macro2::TokenStream;
 use quote::quote;
-use regex::Regex;
 use syn::Field;
 
 use crate::components::get_build::{Builder, get_build};
@@ -10,11 +9,6 @@ pub fn validator_pattern(field: &Field) -> TokenStream {
     let Builder { err, pattern } = get_build(&field.attrs);
 
     if let (Some(pattern), Some(err)) = (pattern.as_ref(), err.as_ref()) {
-        if let Err(e) = Regex::new(pattern) {
-            return syn::Error::new_spanned(field, format!("invalid regex pattern: {}", e))
-                .to_compile_error();
-        }
-
         return quote! {
             {
                 static RE: std::sync::LazyLock<regex::Regex> = std::sync::LazyLock::new(|| {
--- src/components/validator_range.rs
+++ src/components/validator_range.rs
@@ -2,54 +2,32 @@ use super::prelude::*;
 use mokuya::components::prelude::*;
 use proc_macro2::TokenStream;
 use quote::quote;
-use syn::{DeriveInput, Field, Lit};
+use syn::{DeriveInput, Field};
 
 pub fn validator_range(input: &DeriveInput, field: &Field) -> TokenStream {
     let field_name = field.ident.as_ref().expect("field_name must be set");
     let attributes = get_attributes(input);
     let range = get_range(attributes);
 
-    fn try_parse_literal(ts: &TokenStream) -> Option<Lit> {
-        syn::parse2::<Lit>(ts.clone()).ok()
-    }
+    let mut validation_code = TokenStream::new();
 
+    // Check for min and max value and error message.
     if let (Some(min_ts), Some(max_ts)) = (&range.min, &range.max) {
-        if let (Some(min_lit), Some(max_lit)) =
-            (try_parse_literal(min_ts), try_parse_literal(max_ts))
-        {
-            let min_val_opt = match &min_lit {
-                Lit::Int(lit_int) => lit_int.base10_parse::<i128>().ok(),
-                Lit::Float(lit_float) => lit_float.base10_parse::<f64>().ok().map(|f| f as i128),
-                _ => None,
-            };
-            let max_val_opt = match &max_lit {
-                Lit::Int(lit_int) => lit_int.base10_parse::<i128>().ok(),
-                Lit::Float(lit_float) => lit_float.base10_parse::<f64>().ok().map(|f| f as i128),
-                _ => None,
-            };
-
-            if let (Some(min_val), Some(max_val)) = (min_val_opt, max_val_opt) {
-                if min_val > max_val {
-                    return syn::Error::new_spanned(
-                        field,
-                        format!(
-                            "invalid range: min ({}) cannot be greater than max ({})",
-                            min_val, max_val
-                        ),
-                    )
-                    .to_compile_error();
+        if let (Some(min_error), Some(_)) = (&range.err_min, &range.err_max) {
+            // Add a runtime check to ensure min is not greater than max.
+            validation_code.extend(quote! {
+                if #min_ts > #max_ts {
+                    return Err(#min_error); // Or a custom error for range
                 }
-            }
+            });
         }
     }
-
-    let mut validation_code = TokenStream::new();
 
     if let Some(min_ts) = &range.min {
         if let Some(min_error) = &range.err_min {
             validation_code.extend(quote! {
                 if self.#field_name < #min_ts {
-                    return Err(#min_error);
+                    return Err(#min_error.into());
                 }
             });
         }
@@ -59,7 +37,7 @@ pub fn validator_range(input: &DeriveInput, field: &Field) -> TokenStream {
         if let Some(max_error) = &range.err_max {
             validation_code.extend(quote! {
                 if self.#field_name > #max_ts {
-                    return Err(#max_error);
+                    return Err(#max_error.into());
                 }
             });
         }
