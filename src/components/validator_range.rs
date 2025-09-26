68bd9bbe34af02504d3dbe595e22053766f77969
pas2rust <pas2000@proton.me>
2025-09-26 20:01:52 -0300

    0.0.8

--- src/components/validator_range.rs
+++ src/components/validator_range.rs
@@ -12,35 +12,31 @@ pub fn validator_range(input: &DeriveInput, field: &Field) -> TokenStream {
     let mut validation_code = TokenStream::new();
 
     // Check for min and max value and error message.
-    if let (Some(min_ts), Some(max_ts)) = (&range.min, &range.max) {
-        if let (Some(min_error), Some(_)) = (&range.err_min, &range.err_max) {
-            // Add a runtime check to ensure min is not greater than max.
-            validation_code.extend(quote! {
-                if #min_ts > #max_ts {
-                    return Err(#min_error); // Or a custom error for range
-                }
-            });
-        }
+    if let (Some(min_ts), Some(max_ts), Some(min_error), Some(_)) =
+        (&range.min, &range.max, &range.err_min, &range.err_max)
+    {
+        // Add a runtime check to ensure min is not greater than max.
+        validation_code.extend(quote! {
+            if #min_ts > #max_ts {
+                return Err(#min_error); // Or a custom error for range
+            }
+        });
     }
 
-    if let Some(min_ts) = &range.min {
-        if let Some(min_error) = &range.err_min {
-            validation_code.extend(quote! {
-                if self.#field_name < #min_ts {
-                    return Err(#min_error.into());
-                }
-            });
-        }
+    if let (Some(min_ts), Some(min_error)) = (&range.min, &range.err_min) {
+        validation_code.extend(quote! {
+            if self.#field_name < #min_ts {
+                return Err(#min_error.into());
+            }
+        });
     }
 
-    if let Some(max_ts) = &range.max {
-        if let Some(max_error) = &range.err_max {
-            validation_code.extend(quote! {
-                if self.#field_name > #max_ts {
-                    return Err(#max_error.into());
-                }
-            });
-        }
+    if let (Some(max_ts), Some(max_error)) = (&range.max, &range.err_max) {
+        validation_code.extend(quote! {
+            if self.#field_name > #max_ts {
+                return Err(#max_error.into());
+            }
+        });
     }
 
     validation_code
